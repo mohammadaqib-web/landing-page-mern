310acbdcf05a554d3d98d1a508db0dff336b4d50
mohammadaqib-web <mohdaqib918@gmail.com>
2025-11-09 22:53:25 +0530

    added ethereal service

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -11,14 +11,12 @@ export const sendMail = async ({ name, email, message }) => {
     // });
 
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+      host: "smtp.ethereal.email",
+      port: 587,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
-      tls: { rejectUnauthorized: false },
     });
 
     await transporter.sendMail({
